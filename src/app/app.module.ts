7fe0edf5d9c1a3e82568353488b0358b62fab5f1
Lena <lenach241@gmail.com>
2020-07-31 21:39:54 -0500

    feat: add components to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchFieldComponent } from './components/search-field/search-field.component';
+import { VideoListComponent } from './components/video-list/video-list.component';
+import { VideoItemComponent } from './components/video-item/video-item.component';
+import { PageContentComponent } from './components/page-content/page-content.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { SearchSettingsComponent } from './components/search-settings/search-settings.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchFieldComponent,
+    VideoListComponent,
+    VideoItemComponent,
+    PageContentComponent,
+    MainPageComponent,
+    SearchSettingsComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
